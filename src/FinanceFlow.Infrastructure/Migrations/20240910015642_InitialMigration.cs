e7c5664ff60a094843b6ec9d93da1476f14991a4
Gabriel Rieff <gabrielrieff1@gmail.com>
2024-09-25 22:10:49 -0300

    Ajuste na migration

--- src/FinanceFlow.Infrastructure/Migrations/20240910015642_InitialMigration.cs
+++ src/FinanceFlow.Infrastructure/Migrations/20240910015642_InitialMigration.cs
@@ -46,7 +46,7 @@ namespace FinanceFlow.Infrastructure.Migrations
                     Description = table.Column<string>(type: "longtext", nullable: true)
                         .Annotation("MySql:CharSet", "utf8mb4"),
                     Create_at = table.Column<DateTime>(type: "datetime(6)", nullable: false),
-                    Amount = table.Column<decimal>(type: "decimal(65,30)", nullable: false),
+                    Amount = table.Column<decimal>(type: "decimal(65,2)", nullable: false),
                     PaymentType = table.Column<int>(type: "int", nullable: false),
                     UserId = table.Column<long>(type: "bigint", nullable: false)
                 },
