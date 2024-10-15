96ffe4f62a6f198af078c6a20d4b9740b7d4e053
Gabriel Rieff <gabrielrieff1@gmail.com>
2024-10-14 22:43:54 -0300

    test: Adicionado teste para deletar usuario

---
+++ tests/UseCase.Test/Users/Delete/DeleteUserUseCaseTest.cs
@@ -0,0 +1,33 @@
+﻿using commonTestUtilities.Entities;
+using commonTestUtilities.Repositories;
+using commonTestUtilities.Repositories.Users;
+using commonTestUtilities.Services.LoggedUser;
+using FinanceFlow.Application.UseCases.Users.DeleteUser;
+using FinanceFlow.Domain.Entities;
+using FluentAssertions;
+
+namespace UseCase.Test.Users.Delete;
+
+public class DeleteUserUseCaseTest
+{
+    [Fact]
+    public async Task Success()
+    {
+        var user = UserBuilder.Build();
+        var useCase = CreateUseCase(user);
+
+        var act = async () => await useCase.Execute();
+
+        await act.Should().NotThrowAsync();
+    }
+
+
+    private DeleteUserUseCase CreateUseCase(User user)
+    {
+        var repository = UserWhiteOnlyRepositoryBuilder.Build();
+        var initOfWork = UnitOfWorkBuilder.Build();
+        var loggedUser = LoggedUserBuilder.Build(user);
+
+        return new DeleteUserUseCase(repository, initOfWork, loggedUser);
+    }
+}
---
+++ tests/WebApi.Test/Users/Delete/DeleteUserTest.cs
@@ -0,0 +1,24 @@
+﻿using FluentAssertions;
+using System.Net;
+
+namespace WebApi.Test.Users.Delete;
+
+public class DeleteUserTest : FinanceFlowClassFixture
+{
+    private const string Method = "api/User";
+
+    private readonly string _token;
+
+    public DeleteUserTest(CustomWebApplicationFactory webApplicationFactory) : base(webApplicationFactory)
+    {
+        _token = webApplicationFactory._userIdentity.GetToken();
+    }
+
+    [Fact]
+    public async Task Success()
+    {
+        var result = await DoDelete(Method, _token);
+
+        result.StatusCode.Should().Be(HttpStatusCode.NoContent);
+    }
+}
