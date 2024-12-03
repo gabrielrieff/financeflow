584a36f3dfb8cc384e56a9dda2db55bc00e67422
Gabriel Rieff <gabrielrieff1@gmail.com>
2024-12-03 14:32:33 -0300

    feat: correção

---
+++ tests/WebApi.Test/Accounts/Register/RegisterAccountTest.cs
@@ -0,0 +1,34 @@
+﻿//using commonTestUtilities.Requests.Account;
+//using FluentAssertions;
+//using System.Net;
+//using System.Text.Json;
+
+//namespace WebApi.Test.Accounts.Register;
+
+//public class RegisterAccountTest : FinanceFlowClassFixture
+//{
+//    //private const string Method = "api/Account";
+
+//    //private readonly string _token;
+
+//    //public RegisterAccountTest(CustomWebApplicationFactory webApplicationFactory) : base(webApplicationFactory)
+//    //{
+//    //    _token = webApplicationFactory._userIdentity.GetToken();
+//    //}
+
+//    //[Fact]
+//    //public async Task Success()
+//    //{
+//    //    var request = RequestAccountJsonBuilder.Build();
+
+//    //    var result = await DoPost(Method, request, _token);
+
+//    //    result.StatusCode.Should().Be(HttpStatusCode.Created);
+
+//    //    var body = await result.Content.ReadAsStreamAsync();
+
+//    //    var response = await JsonDocument.ParseAsync(body);
+
+//    //    response.RootElement.GetString().Should().Be("Registro concluído");
+//    //}
+//}
---
+++ tests/WebApi.Test/Resouces/AccountIdentityManager.cs
@@ -0,0 +1,18 @@
+﻿using FinanceFlow.Domain.Entities;
+
+namespace WebApi.Test.Resouces;
+
+public class AccountIdentityManager
+{
+    private Account _account;
+
+    public AccountIdentityManager(Account account)
+    {
+        _account = account;
+    }
+
+    public long GetId() => _account.ID;
+
+    public DateTime GetDate() => _account.Create_at;
+
+}
